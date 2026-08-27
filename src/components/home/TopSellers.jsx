76338381c628be0f6eb8ca4207a6fdd7e1fdc4fe
Sebastian Giraldo <sebastiangiraldo96@gmail.com>
2026-08-27 15:51:35 -0500

    feat: TopSellers dynamcally load data, skeleton state

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const TopSellers = () => {
+  const [nfts, setNfts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers",
+    );
+    setNfts(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    fetchData();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +33,67 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading &&
+                new Array(12).fill(null).map((_, index) => (
+                  <li key={index}>
+                    <div style={{ display: "flex" }}>
+                      <div
+                        className="author_list_pp skeleton-box"
+                        style={{
+                          height: "50px",
+                          width: "50px",
+                          borderRadius: "50px",
+                          overflow: "visible",
+                        }}
+                      >
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div
+                        className="author_list_info"
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          paddingLeft: "10px",
+                        }}
+                      >
+                        <div
+                          className="skeleton-box"
+                          style={{ width: "100px", height: "20px" }}
+                        ></div>
+                        <span
+                          className="skeleton-box"
+                          style={{
+                            width: "50px",
+                            height: "20px",
+                            marginTop: "10px",
+                          }}
+                        ></span>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+
+              {nfts &&
+                nfts.map((nft) => (
+                  <li key={nft.id}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${nft.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={nft.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${nft.authorId}`}>
+                        {nft.authorName}
+                      </Link>
+                      <span>{nft.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
